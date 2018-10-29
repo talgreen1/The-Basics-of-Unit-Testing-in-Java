3a8e4ac47e8deea5a7b20c2e7c488fd0d03e3f44
Green, Tal (tg4920) <tg4920@intl.att.com>
2018-10-29 09:16:54 +0200

    Initial Commit

---
+++ src/test/java/_01_unit_test_and_tdd/BookStoreTest.java
@@ -0,0 +1,20 @@
+package _01_unit_test_and_tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookStoreTest {
+
+    @Test
+    public void getBookCount() {
+    }
+
+    @Test
+    public void addBook() {
+    }
+
+    @Test
+    public void getBookById() {
+    }
+}
---
+++ src/test/java/_02_junit_lifecycle/JunitLifeCycle.java
@@ -0,0 +1,44 @@
+package _02_junit_lifecycle;
+
+import org.junit.*;
+
+public class JunitLifeCycle {
+    // MUST be a public method!
+    @Test
+    public void test1() {
+        System.out.println("Running test1!");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Running test2!");
+    }
+
+
+    // Set up and tear down methods:
+
+    // MUST be a public static method!
+    @BeforeClass
+    public static void setUpClass() {
+        System.out.println("Running set up before class...");
+    }
+
+    // MUST be a public static method!
+    @AfterClass
+    public static void tearDownClass() {
+        System.out.println("Running tear down after class...");
+    }
+
+    // MUST be a public method!
+    // Multiple methods are supported but order is not guaranteed - so use only one!
+    @Before
+    public void setUp() {
+        System.out.println("Running set up before test...");
+    }
+
+    // MUST be a public method!
+    @After
+    public void tearDown() {
+        System.out.println("Running tear down after test...");
+    }
+}
