dcc0a28d9d3cba29a560d60709bf2c858590b492
Green, Tal (tg4920) <tg4920@intl.att.com>
2018-11-13 08:10:23 +0200

    Rename tests names

--- javaUnitTest-examples/src/test/java/com/junitcourse/_01_unit_test_and_tdd/StringUtilsTest.java
+++ javaUnitTest-examples/src/test/java/com/junitcourse/_01_unit_test_and_tdd/StringUtilsTest.java
@@ -6,20 +6,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringUtilsTest {
     @Test
-    public void twoValidStrings() {
+    public void Given_2_strings_When_calling_concatLowerCase_Then_you_get_a_concatination_of_them_both() {
         StringUtils utils = new StringUtils();
-        String str1 = "hello";
-        String str2 = "world";
+        String str1 = "Hello";
+        String str2 = "WORLD";
         String result = utils.concatLowerCase(str1, str2);
         assertThat(result).isEqualTo("helloworld");
     }
 
     @Test
-    public void oneNullString() {
+    public void Given_one_string_and_one_null_When_calling_concatLowerCase_Then_you_get_the_string_in_lowercase() {
         StringUtils utils = new StringUtils();
-        String result = utils.concatLowerCase("hello", null);
+        String result = utils.concatLowerCase("HELLO", null);
         assertThat(result).isEqualTo("hello");
-        result = utils.concatLowerCase(null, "hello");
+        result = utils.concatLowerCase(null, "HELLO");
         assertThat(result).isEqualTo("hello");
     }
 
