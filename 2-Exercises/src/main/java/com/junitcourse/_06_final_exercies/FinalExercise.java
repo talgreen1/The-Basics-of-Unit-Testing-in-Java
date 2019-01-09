c3023bc20f93dc883793d38a8609ba7910fb2e26
Green, Tal (tg4920) <tg4920@intl.att.com>
2019-01-09 14:18:22 +0200

    Add Final Exercise

---
+++ 2-Exercises/src/main/java/com/junitcourse/_06_final_exercies/FinalExercise.java
@@ -0,0 +1,13 @@
+package com.junitcourse._06_final_exercies;
+
+public class FinalExercise {
+
+    /**
+     * Add unit tests for the following methods of BookStore:
+     * - getBooksByAuthor
+     * - updateBook
+     * - removeBookById
+     *
+     * You can create class for each method - 3 classes in total
+     */
+}
